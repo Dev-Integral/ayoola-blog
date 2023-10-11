2647b7fcdc16e99e8ad0298ac6fc9ea26c97c90c
Taiwo Ayoola <ayoola@glade.ng>
2023-10-11 15:26:36 +0100

    Removed unused codes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Nav from "./component/Nav";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Details from "./pages/Details";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
