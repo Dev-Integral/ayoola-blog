c620aece78ea8277b7c7d0345001738f24e8fa0e
Taiwo Ayoola <ayoola@glade.ng>
2024-02-06 22:43:56 +0100

    updated url

--- src/redux/hadid/auth/authActions.js
+++ src/redux/hadid/auth/authActions.js
@@ -4,7 +4,7 @@ import { GET_AUTH_START } from "./type";
 export const getAuth = () => {
     return (dispatch) => {
       dispatch({ type: GET_AUTH_START });
-      axios.post("https://powered-by-bamboo-sandbox.investbamboo.com/", {username: "test", password: "test"})
+      axios.post("https://powered-by-bamboo-sandbox.investbamboo.com/oauth/token", {username: "test", password: "test"})
         .then((res) => {
             console.log("res", res);
         })
