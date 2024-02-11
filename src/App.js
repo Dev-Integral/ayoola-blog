464d42f58f7ba55aa9bbf6e14298de84fb59c6ba
Taiwo Ayoola <ayoola@glade.ng>
2024-02-11 23:55:55 +0100

    Started admin section.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import store from "./redux/store";
 import Details from "./pages/Details";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/details/:id" element={<Details />} />
+          <Rote path="/admin/posts" element={<AdminDashboard />} />
           <Route path="*" element={<Navigate to="/home" replace />} />
         </Routes>
       </Router>
